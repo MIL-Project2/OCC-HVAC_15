e9c9463275b1186400ff6c2b8cc4beab28df96ca
MIL-Project2 <jwas0006@student.monash.edu>
2026-09-15 02:59:55 +1000

    Update feature_engineering.py

--- feature_engineering.py
+++ feature_engineering.py
@@ -34,8 +34,10 @@ ROLLING_WINDOW_STEPS = 3                                       # 15 min
 MODEL_FEATURES = [
     'headcount',
     'headcount_rolling_15min',
-    'working_weekday_hour_future'
-    ]
+    'is_weekend_future', 'working_weekday_hour_future',
+    'hour_sin_future', 'hour_cos_future',
+    'day_sin_future', 'day_cos_future',
+]
 TARGET_COLUMN = 'headcount_target_15min'
 
 REQUIRED_RAW_COLUMNS = ['collecteddate', 'headcount']
